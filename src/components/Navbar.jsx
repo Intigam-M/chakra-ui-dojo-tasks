3cc6afc89647401515e337105e8a807cde714ae0
Intigam Mammadli <intigam.mammadli@gmail.com>
2024-01-07 12:50:26 +0400

    Update Navbar and RootLayout components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,22 +2,15 @@ import { Flex, Heading, Box, Text, Button, Spacer, HStack } from "@chakra-ui/rea
 
 export default function Navbar() {
   return (
-    <Flex as="nav" p="10px" alignItems="center">
-      <Heading as="h1">Dojo Tasks</Heading>
+    <Flex as="nav" p="10px" mb="60px" alignItems="center">
+      <Heading as="h1" fontSize="1.5em" color='GrayText'>Dojo Tasks</Heading>
       <Spacer />
 
       <HStack spacing="20px"> 
-        <Box bg="gray.200" p="10px">M</Box>
-        <Text>mario@netninja.dev</Text>
+        <Box bg="gray.200" p="10px 15px" borderRadius="50%">M</Box>
+        <Text color="GrayText">mario@netninja.dev</Text>
         <Button colorScheme="purple">Logout</Button>
       </HStack>
     </Flex>
-
-    // <Flex bg="gray.200" justify="space-between" wrap="wrap" gap={2}>
-    //   <Box w="150px" h="50px" bg="red">1</Box>
-    //   <Box w="150px" h="50px" bg="blue">2</Box>
-    //   <Box w="150px" flexGrow="1" h="50px" bg="green">3</Box>
-    //   <Box w="150px" flexGrow="2" h="50px" bg="yellow">4</Box>
-    // </Flex>
   )
 }
