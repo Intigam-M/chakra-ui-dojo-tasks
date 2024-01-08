31c75b878d242802780e56ee4394a801490d158d
Intigam Mammadli <intigam.mammadli@gmail.com>
2024-01-08 19:41:45 +0400

    Add Chakra UI Icons and JSON Server dependencies, and update RootLayout and Dashboard components

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -3,30 +3,31 @@ import { Outlet } from "react-router-dom"
 
 // components
 import Navbar from "../components/Navbar"
+import Sidebar from "../components/Sidebar"
 
 export default function RootLayout() {
-  return (
-    <Grid templateColumns="repeat(6, 1fr)" bg="gray.50">
-      {/* sidebar */}
-      <GridItem
-        as="aside"
-        colSpan={{ base: 6, lg: 2, xl: 1 }} 
-        bg="purple.400"
-        minHeight={{ lg: '100vh' }}
-        p={{ base: '20px', lg: '30px' }}
-      >
-        <span>sidebar</span>
-      </GridItem>
+    return (
+        <Grid templateColumns="repeat(6, 1fr)" bg="gray.50">
+            {/* sidebar */}
+            <GridItem
+                as="aside"
+                colSpan={{ base: 6, lg: 2, xl: 1 }}
+                bg="purple.400"
+                minHeight={{ lg: '100vh' }}
+                p={{ base: '20px', lg: '30px' }}
+            >
+                <Sidebar />
+            </GridItem>
 
-      {/* main content & navbar */}
-      <GridItem
-        as="main"
-        colSpan={{ base: 6, lg: 4, xl: 5 }} 
-        p="40px"
-      >
-        <Navbar />
-        <Outlet />
-      </GridItem>
-    </Grid>
-  )
+            {/* main content & navbar */}
+            <GridItem
+                as="main"
+                colSpan={{ base: 6, lg: 4, xl: 5 }}
+                p="40px"
+            >
+                <Navbar />
+                <Outlet />
+            </GridItem>
+        </Grid>
+    )
 }
